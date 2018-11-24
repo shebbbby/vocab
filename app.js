d2295d1e2e9486c344e2105211710d6b3583378f
daniel shebshayevich <dsheb12@gmail.com>
2018-11-24 18:13:24 -0500

    My first commit

--- app.js
+++ app.js
@@ -556,11 +556,11 @@ app.get('/logout', (req, res, next) => {
     res.redirect('/login');
 });
 // Tells Express that we want to start accepting connections
-app.listen(process.env.PORT || 3000, function(err){
+app.listen(process.env.PORT || 5000, function(err){
     if (err) {
         console.log(err);
     } else {
-        console.log("Listening on port on 3000");
+        console.log("Listening on port on 5000");
     }
 });
 
